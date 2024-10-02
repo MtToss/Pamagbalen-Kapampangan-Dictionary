efd3720a06a42d440b299fdddcacacef19cccb85
John Cedric <cherrypogiced@gmail.com>
2024-10-02 23:28:21 +0800

    read file

---
+++ src/main/python/test.py
@@ -0,0 +1,13 @@
+import csv
+
+csv_file = 'kapampangan_translations.csv'
+
+
+with open(csv_file, mode='r', newline='', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    
+    next(reader)
+    
+    for row in reader:
+        kapampangan_word, tagalog_word, english_word = row
+        print(f"Kapampangan: {kapampangan_word} | Tagalog: {tagalog_word} | English: {english_word}")
